ba0f455137a5876caab71672816a968e66fce242
agroce <agroce@gmail.com>
2021-01-31 06:53:47 -0700

    yul comp

---
+++ shrink.py
@@ -0,0 +1,54 @@
+import subprocess
+import os
+import shutil
+
+def bug():
+    with open("triage.out", 'w') as tfile:
+        r = subprocess.call(["target/debug/fe code.fe"], shell=True, stdout=tfile, stderr=tfile)
+    m = None
+    with open("triage.out", 'r') as tfile:
+        for line in tfile:
+            if "thread" in line:
+                ms = line.split("'")
+                for mc in ms:
+                    if ".rs" in mc and "message" not in line:
+                        m = mc
+                        break
+                    if "message" in mc:
+                        m = "Yul compilation failed:" + mc.split('"message":')[1].split('"severity":')[0]
+                        break
+    return m
+
+dnull = open(os.devnull, 'w')
+
+code = []
+
+with open("start.fe", 'r') as f:
+    code = f.readlines()
+
+shutil.copy("start.fe", "code.fe")
+initial_bug = bug()
+    
+changed = True
+while changed:
+    changed = False
+    for pos in range(len(code)):
+        for stride in range(1,3):
+            with open("code.fe", 'w') as f:
+                i = 0
+                omitted = range(pos,pos+stride)
+                for line in code:
+                    if i not in omitted:
+                        f.write(line)
+                    i += 1
+            new_bug = bug()
+            if new_bug == initial_bug:
+                print("REMOVING LINE", pos, "STRIDE", stride, "WORKED:")
+                print("NEW LENGTH:", len(code)-1)
+                changed = True
+                newcode = code[:pos] + code[pos+stride:]
+                code = newcode
+                with open("finish.fe", 'w') as f:
+                    for line in code:
+                        f.write(line)
+                break
---
+++ triage.py
@@ -0,0 +1,39 @@
+import subprocess
+import os
+import sys
+import glob
+import shutil
+
+dnull = open(os.devnull, 'w')
+
+triage = {}
+
+for f in glob.glob(sys.argv[1]):
+    size = os.stat(f).st_size
+    shutil.copy(f, "code.fe")
+    with open("triage.out", 'w') as tfile:
+        r = subprocess.call(["target/debug/fe code.fe"], shell=True, stdout=tfile, stderr=tfile)
+    with open("triage.out", 'r') as tfile:
+        for line in tfile:
+            if "thread" in line:
+                if "not implemented" in line:
+                    break
+                ms = line.split("'")
+                for mc in ms:
+                    if ".rs" in mc and "message" not in line:
+                        m = mc
+                        break
+                    if "message" in mc:
+                        m = "Yul compilation failed:" + mc.split('"message":')[1].split('"severity":')[0]
+                        break
+                if m not in triage:
+                    triage[m] = (f, 1, size)
+                else:
+                    (ofile, count, osize) = triage[m]
+                    if (size < osize):
+                        triage[m] = (f, count+1, size)
+                    else:
+                        triage[m] = (ofile, count+1, osize)
+
+for t in triage:
+    print(t, triage[t])
