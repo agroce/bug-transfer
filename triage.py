122a0a02b6e1f9b8b2ce315ecabc3f4d443481a7
agroce <agroce@gmail.com>
2021-03-01 09:01:06 -0700

    avoid some closed

--- triage.py
+++ triage.py
@@ -8,6 +8,33 @@ dnull = open(os.devnull, 'w')
 
 triage = {}
 
+for f in glob.glob("bug-transfer/known_closed/*.fe"):
+    size = os.stat(f).st_size
+    shutil.copy(f, "code.fe")
+    with open("triage.out", 'w') as tfile:
+        r = subprocess.call(["target/debug/fe code.fe"], shell=True, stdout=tfile, stderr=tfile)
+    with open("triage.out", 'r') as tfile:
+        for line in tfile:
+            if "thread" in line:
+                if "not implemented" in line:
+                    break
+                ms = line.split("'")
+                for mc in ms:
+                    if ".rs" in mc and "message" not in line:
+                        m = mc
+                        break
+                    if "message" in mc:
+                        m = "Yul compilation failed:" + mc.split('"message":')[1].split('"severity":')[0]
+                        break
+                if m not in triage:
+                    triage[m] = (f, 1, size)
+                else:
+                    (ofile, count, osize) = triage[m]
+                    if (size < osize):
+                        triage[m] = (f, count+1, size)
+                    else:
+                        triage[m] = (ofile, count+1, osize)
+
 for f in glob.glob("bug-transfer/known_open/*.fe"):
     size = os.stat(f).st_size
     shutil.copy(f, "code.fe")
@@ -26,6 +53,8 @@ for f in glob.glob("bug-transfer/known_open/*.fe"):
                     if "message" in mc:
                         m = "Yul compilation failed:" + mc.split('"message":')[1].split('"severity":')[0]
                         break
+                if "Variable name " in m and " already taken in this scope" in m:
+                    m = 'Yul compilation failed:"Variable name $FOO already taken in this scope.'
                 if m not in triage:
                     triage[m] = (f, 1, size)
                 else:
@@ -63,9 +92,10 @@ for f in glob.glob(sys.argv[1]):
                         triage[m] = (ofile, count+1, osize)
 
 for t in triage:
-    if "Switch statement without any cases" in t:
+    if "Bad escape sequence" in t: # Needs special casing to ignore
         continue
-    if "already taken in this scope" in t:
-        continue
+
     if "known_open" not in triage[t][0]:
+        if "known_closed" in triage[t][0]:
+            print("KNOWN CLOSED:", t, triage[t])
         print(t, triage[t])
